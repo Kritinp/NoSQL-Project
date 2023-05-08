fd82dc0401b17ebd0854303c93b48b6aca6df338
avantika <avantika.mudumbai@gmail.com>
2023-05-08 23:30:00 +0530

    language detection: without pig

---
+++ lang/src/com/example/LanguageDetection.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+
+public class LanguageDetection {
+    public static void main(String[] args) {
+        try {
+            DetectorFactory.loadProfile("profiles");
+            Detector detector = DetectorFactory.create();
+            detector.append("This is some text in English.");
+            String language = detector.detect();
+
+            System.out.println("Detected language: " + language);
+        } catch (LangDetectException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
